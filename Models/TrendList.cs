a259c0c128080fde1861a404f5509772c6bbb0de
epkimtaiho <epkim@taihoinst.com>
2018-03-12 17:20:42 +0900

    TrendList

---
+++ Models/TrendList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortChatBot.Models
+{
+
+    [Serializable]
+    public class TrendList
+    {
+        public string year;
+        public string month;
+        public string eqp_typ;
+        public string accidenttype;
+        public string count;
+
+    }
+}
