77c2b9b34c0ba822d5180ba3c5657132dbe7f38c
taihoinst01 <gjlim@taihoinst.com>
2018-05-03 16:19:06 +0900

    QueryIntentList.cs
    QueryIntentList.cs

---
+++ Models/QueryIntentList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortChatBot.Models
+{
+    public class QueryIntentList
+    {
+        public string luisId;
+        public string luisIntent;
+        public int dlgId;
+    }
+}
